f88b2d2d9c7dc9c8e4d53fd31f83f357a6088f62
tasmiyafathima <you@example.com>
2021-05-29 17:47:56 +0530

    commit

---
+++ bolt project/script.js
@@ -0,0 +1,10 @@
+var elem = document.querySelector('.grid-slider-wrap');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true,
+  groupCells:true,
+  wrapAround:true,
+  pageDots:false,
+  autoPlay: true
+});
